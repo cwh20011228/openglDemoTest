6d1317e345d82e923f0a52516042d791ae48fa96
cwh20011228 <3045696670@qq.com>
2026-01-25 21:06:43 +0800

    离屏的msaa

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
+ #include <iostream>
 #include <vector>
 #include <string>
 #include <initializer_list>
+#include <algorithm>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
@@ -343,8 +344,254 @@ void main() {
 }
 )GLSL";
 
+const std::string quadVertexShaderSource = R"GLSL(
+#version 460 core
+layout (location = 0) in vec2 aPos;
+layout (location = 1) in vec2 aTexCoords;
+out vec2 TexCoords;
+void main() {
+    TexCoords = aTexCoords;
+    gl_Position = vec4(aPos.x, aPos.y, 0.0, 1.0); 
+}
+)GLSL";
 
 
+const std::string quadFragmentShaderSource = R"GLSL(
+#version 460 core
+out vec4 FragColor;
+in vec2 TexCoords;
+uniform sampler2D screenTexture;
+void main() {
+    FragColor = texture(screenTexture, TexCoords);
+}
+)GLSL";
+
+/*
+* fbo的MSAA
+*/
+#if 1
+int main(int argc, char* argv[]) {
+    // (1)初始化GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // (2)创建窗口
+    GLFWwindow* window = glfwCreateWindow(windowsWidth, windowsHeight, "OpenGL FBO MSAA Demo", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // (3)加载OpenGL函数指针
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // 初始化时手动同步一次 viewport（防止回调还没触发）
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    int maxSamples = 0;
+    glGetIntegerv(GL_MAX_SAMPLES, &maxSamples);
+	std::cout << "GL_MAX_SAMPLES = " << maxSamples << std::endl;
+
+	// 初始化多重采样FBO
+    // 使用Renderbuffer（RBO）作为颜色附件，因为读写性能在大多数硬件上都优于纹理附件
+    unsigned int msaaFBO, rbo;
+	int samples = std::min(4, maxSamples); // 使用4x MSAA，当然也可以使用更高的采样数
+
+    glGenFramebuffers(1, &msaaFBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, msaaFBO);
+
+    // 创建多重采样渲染缓冲对象
+    glGenRenderbuffers(1, &rbo);
+    glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+	// 分配多重采样存储
+    glRenderbufferStorageMultisample(GL_RENDERBUFFER, samples, GL_RGBA, width, height);
+    // 将RBO挂载到FBO的颜色附件上
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, rbo);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+		std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0); // 解绑FBO
+
+
+	// 创建一个中间FBO，用于将多重采样结果解析到普通纹理
+    // 由于多重采样的RBO无法直接被着色器读取，需要第二个普通的FBO，它挂在一个标准的2D纹理
+    unsigned int intermediateFBO, screenTexture;
+    glGenFramebuffers(1, &intermediateFBO);
+    glBindFramebuffer(GL_FRAMEBUFFER, intermediateFBO);
+
+	// 创建2D纹理作为中间FBO的颜色附件
+	glGenTextures(1, &screenTexture);
+    glBindTexture(GL_TEXTURE_2D, screenTexture);
+    glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,width,height,0,GL_RGBA,GL_UNSIGNED_BYTE,nullptr);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // 将纹理挂载到中转fbo
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, screenTexture, 0);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+		std::cout << "ERROR::FRAMEBUFFER:: Intermediate Framebuffer is not complete!" << std::endl;
+    }
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    // 绘制指令
+    glBindFramebuffer(GL_FRAMEBUFFER, msaaFBO);
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+         
+    GLuint program = CreateProgramFromSources(
+        {
+            {GL_VERTEX_SHADER,   &vertexShaderSource},
+		    {GL_FRAGMENT_SHADER, &fragmentShaderSource},
+        }
+    );
+
+    GLuint quadProgram = CreateProgramFromSources(
+        {
+            {GL_VERTEX_SHADER,   &quadVertexShaderSource},
+            {GL_FRAGMENT_SHADER, &quadFragmentShaderSource},
+        }
+        );
+
+    if (!program && !quadProgram) {
+        std::cerr << "Failed to create shader program" << std::endl;
+        return -1;
+    }
+
+    /*
+	* msaa的四边形VAO / VBO
+    */
+    float vertexs[] = {
+        -1.0f,  1.0f,  0.0f, 
+        -1.0f, -1.0f,  0.0f, 
+         1.0f, -1.0f,  0.0f,
+        -1.0f,  1.0f,  0.0f, 
+         1.0f, -1.0f,  0.0f, 
+         1.0f,  1.0f,  0.0f,
+    };
+
+    float colors[] = {
+        1.0f,0.0f,0.0f,
+        1.0f,0.0f,0.0f,
+        1.0f,0.0f,0.0f,
+        1.0f,0.0f,0.0f,
+        1.0f,0.0f,0.0f,
+        1.0f,0.0f,0.0f,
+    };
+
+    unsigned int vertextVbo, colorVbo, vao;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vertextVbo);
+    glGenBuffers(1, &colorVbo);
+
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertextVbo);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertexs),vertexs,GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, colorVbo);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(colors),colors,GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vertextVbo);
+    glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, colorVbo);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    /* msaa的四边形VAO / VBO */
+
+	/*
+    * 全屏四边形VAO / VBO
+    */ 
+    unsigned int quadVAO, quadVBO;
+
+    float quadVertices[] = {
+        // 位置 (x, y) 纹理坐标 (u, v)
+        -0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  1.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  1.0f, 1.0f
+    };
+
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+
+    // 设置位置属性 (location = 0)
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    // 设置纹理坐标属性 (location = 1)
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+    /* 全屏四边形VAO / VBO */
+
+
+    // 主循环
+    while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        
+		// 1. 渲染场景到多重采样FBO
+        glBindFramebuffer(GL_FRAMEBUFFER, msaaFBO);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(program);
+		glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+
+		// 2. 将多重采样FBO解析到中间FBO
+		glBindFramebuffer(GL_READ_FRAMEBUFFER, msaaFBO);    // 读取源
+		glBindFramebuffer(GL_DRAW_FRAMEBUFFER, intermediateFBO);    // 写入目标
+        glBlitFramebuffer(0,0,width,height,
+                          0, 0, width, height,
+		                  GL_COLOR_BUFFER_BIT, GL_NEAREST);     // 帧缓冲的内容拷贝（解析）
+
+
+        // 3.渲染解析后的纹理到屏幕
+		glBindFramebuffer(GL_FRAMEBUFFER, 0); // 绑定默认帧缓冲
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(quadProgram);
+
+        glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, screenTexture);    // screenTexture是中间FBO的颜色附件纹理
+		glUniform1i(glGetUniformLocation(quadProgram, "screenTexture"), 0);    // 绑定纹理单元0，从0号纹理单元采样
+        
+        glBindVertexArray(quadVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
+        
+        glfwSwapBuffers(window);
+		glfwPollEvents();
+    }
+
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vertextVbo);
+    glDeleteBuffers(1,&colorVbo);
+    glfwDestroyWindow(window);
+	glfwTerminate();
+
+    return 0;
+}
+#endif
+
+/*
+* 默认帧缓冲的MSAA
+*/
+#if 0
 int main(int argc, char* argv[]) {
     // (1)初始化GLFW
 	glfwInit();
@@ -458,3 +705,5 @@ int main(int argc, char* argv[]) {
     glfwTerminate();
 	return 0;
 }
+
+#endif
